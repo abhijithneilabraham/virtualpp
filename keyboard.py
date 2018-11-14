6ff729006325dad6d93c3967de2cb3144a244dc8
abhijithneilabraham <abhijithneilabrahampk@gmail.com>
2018-11-15 00:08:20 +0530

    Update keyboard.py

---
+++ keyboard.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 14 23:48:32 2018
+
+@author: abhijithneilabraham
+"""
+
+import pyautogui
+a=pyautogui.press('left')
+d=pyautogui.press('right')
+w=pyautogui.press('up')
+s=pyautogui.press('down')
