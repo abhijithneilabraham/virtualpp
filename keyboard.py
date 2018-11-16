db4f206a3211ef809d63da3b353c6decfcf5283b
abhijithneilabraham <abhijithneilabrahampk@gmail.com>
2018-11-16 20:32:04 +0530

    Update keyboard.py

--- keyboard.py
+++ keyboard.py
@@ -7,6 +7,6 @@ Created on Wed Nov 14 23:48:32 2018
 """
 
 import pyautogui
-
-w=pyautogui.press('up')
-s=pyautogui.press('down')
+pyautogui.moveTo(100, 200,.01)
+pyautogui.moveTo(None, 500)  # moves mouse to X of 100, Y of 500.
+pyautogui.moveTo(600, None)
