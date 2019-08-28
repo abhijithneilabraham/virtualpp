c68adac3e00e6476a76a72393a0cdd92593c708b
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-08-28 22:36:01 +0530

    Update final.py

--- build/lib/virtualpp/final.py
+++ build/lib/virtualpp/final.py
@@ -11,8 +11,6 @@ import cv2
 import random
 import pygame
 from pygame.locals import *
-
-cap = cv2.VideoCapture(0) #This enables the camera.The value 1 is for external camera,0 for internal camera
 p=100
 i=0
 pygame.init()
@@ -38,10 +36,12 @@ paddle1_vel = 0
 paddle2_vel = 0
 l_score = 0
 r_score = 0
-
-#canvas declaration
-window = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
-pygame.display.set_caption('Hello World')
+def initialize():
+    global cap,window
+    cap = cv2.VideoCapture(0) #This enables the camera.The value 1 is for external camera,0 for internal camera    
+    #canvas declaration
+    window = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
+    pygame.display.set_caption('Ping Pong')
 
 # helper function that spawns a ball, returns a position vector and a velocity vector
 # if right is True, spawn to the right, else spawn to the left
@@ -123,11 +123,6 @@ def draw(canvas):
         ball_vel[0] = -ball_vel[0]
         ball_vel[0] *= 1.3
         ball_vel[1] *= 1.3
-        '''
-    elif int(ball_pos[0]) >= WIDTH + 1 - BALL_RADIUS - PAD_WIDTH:
-        l_score += 1
-        ball_init(False)
-        '''
 
     #update scores
     myfont1 = pygame.font.SysFont("Comic Sans MS", 20)
@@ -169,91 +164,73 @@ def keyup(event):
 
 init()
 
-
-#game loop
-while True:
-
-    
-
-    
-    ret, frame = cap.read()  #returns ret=either true or false. the frame variable has the frames 
-    
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) 
-    '''
-    why did we convert to hsv ? because while using BGR,we have to use 3 values for color combinations(eg.(255,90,30))
-    But in case of hsv,for getting the required color,we have to change only one parameter,that is ,hue ,and the two other parameters can be changed just to change the saturation and values.
-    '''
-    blur = cv2.GaussianBlur(hsv,(5,5),5)
-    '''
-    The blurring is set to high here. we did blurring because,blurring eliminates the image noise and  reduce detail.
-    '''
-    
-    lower_orange = np.array([0,150,150])#see,the hue from 0,30 is used here for orange,but remeber to set the other values too according to your need.If the other two values are zero,then you will get black because of zero intensity
-    upper_orange = np.array([30,255,255])
-    mask = cv2.inRange(blur, lower_orange, upper_orange)
-    '''
-    the color from lower orange to upper orange is noted here.
-    '''
-    ret, thresh_img = cv2.threshold(mask,91,255,cv2.THRESH_BINARY)
-    '''
-    
-    
-    
-    Here, the matter is straight forward. 
-    If pixel value is greater than a threshold value, it is assigned one value (may be white), else it is assigned another value (may be black). 
-    The function used is cv.threshold. First argument is the source image, which should be a grayscale image. 
-    Second argument is the threshold value which is used to classify the pixel values. 
-    Third argument is the maxVal which represents the value to be given if pixel value is more than (sometimes less than) the threshold value. 
-    OpenCV provides different styles of thresholding and it is decided by the fourth parameter of the function. 
-    
-    
-    
-    
-    '''
-    
-    contours =  cv2.findContours(thresh_img,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)[-2]#ah,the cool fun part!draw contours over the required orange area!
-    '''
-    I gave the parameter thresh_img here as input. If I gave frame as input I would get contours for every shape in the camera.
-    But by masking and thresholding, I have concentrated my interests into just the orange area .
-    '''
-    M = cv2.moments(mask)
-    if M["m00"]!=0 :
-        cX = int(M["m10"] / M["m00"])
-        cY = int(M["m01"] / M["m00"])
-#These moments and cX and cY ,i used to draw centroids.I will be using the centroid of the orange area.
-    cv2.circle(frame, (cX, cY), 5, (255, 255, 255), -1)
-    cv2.putText(frame, "centroid", (cX - 25, cY - 25),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-    for c in contours:
-        cv2.drawContours(frame, [c], -1, (0,255,0), 3)#in previous function of contours i just only found contours. Now here I am drawing
-   
-    p=200
-    i=0
-    if cY>=p:
-        for j in range(int((cY-p)/10)):
-            keydown(1)
-            
-        p=cY
-    else:
-        for j in range(int((p-cY)/10)):
-            keyup(1)     
-        p=cY
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    draw(window)
-    '''
-    for event in pygame.event.get():
-
-        if event.type == KEYDOWN:
-            keydown(event)
-        elif event.type == KEYUP:
-            keyup(event)
-        elif event.type == QUIT:
-            pygame.quit()
-            sys.exit()
+def game(ColorLow,ColorHigh):
+    #game loop
+    cX=25
+    cY=25
+    while True:    
+        ret, frame = cap.read()  #returns ret=either true or false. the frame variable has the frames 
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) 
+        '''
+        why did we convert to hsv ? because while using BGR,we have to use 3 values for color combinations(eg.(255,90,30))
+        But in case of hsv,for getting the required color,we have to change only one parameter,that is ,hue ,and the two other parameters can be changed just to change the saturation and values.
+        '''
+        blur = cv2.GaussianBlur(hsv,(5,5),5)
+        '''
+        The blurring is set to high here. we did blurring because,blurring eliminates the image noise and  reduce detail.
+        '''
+        lower_range = np.array(ColorLow)#see,the hue from 0,30 is used here for orange,but remeber to set the other values too according to your need.If the other two values are zero,then you will get black because of zero intensity
+        upper_range = np.array(ColorHigh)
+        mask = cv2.inRange(blur, lower_range, upper_range)
+        '''
+        the color from lower orange to upper orange is noted here.
+        '''
+        ret, thresh_img = cv2.threshold(mask,91,255,cv2.THRESH_BINARY)
+        '''
+        Here, the matter is straight forward. 
+        If pixel value is greater than a threshold value, it is assigned one value (may be white), else it is assigned another value (may be black). 
+        The function used is cv.threshold. First argument is the source image, which should be a grayscale image. 
+        Second argument is the threshold value which is used to classify the pixel values. 
+        Third argument is the maxVal which represents the value to be given if pixel value is more than (sometimes less than) the threshold value. 
+        OpenCV provides different styles of thresholding and it is decided by the fourth parameter of the function. 
+        '''
+        contours =  cv2.findContours(thresh_img,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)[-2]#ah,the cool fun part!draw contours over the required orange area!
+        '''
+        I gave the parameter thresh_img here as input. If I gave frame as input I would get contours for every shape in the camera.
+        But by masking and thresholding, I have concentrated my interests into just the orange area .
+        '''
+        M = cv2.moments(mask)
+        if M["m00"]!=0 :
+            cX = int(M["m10"] / M["m00"])
+            cY = int(M["m01"] / M["m00"])
+        #These moments and cX and cY ,i used to draw centroids.I will be using the centroid of the orange area.
+        cv2.circle(frame, (cX, cY), 5, (255, 255, 255), -1)
+        cv2.putText(frame, "centroid", (cX - 25, cY - 25),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+        for c in contours:
+            cv2.drawContours(frame, [c], -1, (0,255,0), 3)#in previous function of contours i just only found contours. Now here I am drawing
+       
+        p=200
+        i=0
+        if cY>=p:
+            for j in range(int((cY-p)/10)):
+                keydown(1)
+                
+            p=cY
             '''
+            Simulating down arrow pressing when the centroid moves down
+            '''
+        else:
+            for j in range(int((p-cY)/10)):
+                keyup(1)     
+            p=cY
+            '''
+            Simulating up arrow pressing when the centroid moves up
+            '''
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        draw(window)
+        pygame.display.update()
+        fps.tick(100)
         
-    pygame.display.update()
-    fps.tick(100)
-    
-cap.release()#release the camera beast
-cv2.destroyAllWindows()
+    cap.release()#release the camera beast
+    cv2.destroyAllWindows()
